6e7dbd442e410e9fa2ee6cb1153baeae978272e7
deawyk <deawyk@gmail.com>
2021-04-11 12:14:49 -0400

    Update yfinance_active.py

--- data pipeline/yfinance_active.py
+++ data pipeline/yfinance_active.py
@@ -1,10 +1,23 @@
+def stockinfo_to_csv (tickerStrings, file_name):
+    df_list = list()
+    for ticker in tickerStrings:
+        data = yf.download(ticker, group_by="Ticker", period='5y')
+        data['Ticker'] = ticker  #add column with ticker
+        df_list.append(data)
+        
+    #combine all dataframes into a single dataframe
+    df = pd.concat(df_list)
+    
+    #save to csv
+    df.to_csv(file_name)
+
 #Webscraping Yahoo! Finance-Most Active Stocks with Beautiful Soup
 from bs4 import BeautifulSoup
 import requests
 
 url = 'https://finance.yahoo.com/most-active'
 header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'
-}
+         }
 response = requests.get(url, headers=header)
 soup = BeautifulSoup(response.content, 'lxml')
 
@@ -14,19 +27,4 @@ for item in soup.select('.simpTblRow'):
     symbols.append(item.select('[aria-label=Symbol]')[0].get_text())
 
 #Output stock info to csv with yfinance
-
 stockinfo_to_csv(symbols, 'ticker_active.csv')
-
-def stockinfo_to_csv (tickerStrings, file_name):
-tickerStrings = 
-df_list = list()
-for ticker in tickerStrings:
-    data = yf.download(ticker, group_by="Ticker", period='5y')
-    data['Ticker'] = ticker  #add column with ticker
-    df_list.append(data)
-
-#combine all dataframes into a single dataframe
-df = pd.concat(df_list)
-
-#save to csv
-df.to_csv(file_name)
